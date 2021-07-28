44d085e712e239f7a3c1841c1b3ce8ab99337acd
qiaochao <752716886@qq.com>
2021-07-28 17:44:37 +0800

    update

--- 5-3-stack-queue-有效的括号.js
+++ 5-3-stack-queue-有效的括号.js
@@ -13,3 +13,53 @@
 输入: "(]"
 输出: false 
 */
+//版本一比较好理解
+var isValid1 = function (s) {
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        let c = s[i];
+        switch (c) {
+        case '(':
+            stack.push(')');
+            break;
+        case '[':
+            stack.push(']');
+            break;
+        case '{':
+            stack.push('}');
+            break;
+        default:
+            if (c !== stack.pop()) {
+            return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
+
+
+// 简化版本
+var isValid2 = function(s) {
+    const stack = [], 
+        map = {
+            "(":")",
+            "{":"}",
+            "[":"]"
+        };
+    for(const x of s) {
+        //x是key,将key存入stack,跳出本次循环
+        //eg：第一次push的是-(
+        if(x in map) {
+            stack.push(x);
+            //前边的如果push上了就跳过本次循环
+            continue;
+        };
+
+        //第二次进来-),我们通过第一次的key拿到)和x做对比
+        if(map[stack.pop()] !== x) return false;
+    }
+    return !stack.length;
+};
+
+let s = "()[]{}";
+isValid2(s);
