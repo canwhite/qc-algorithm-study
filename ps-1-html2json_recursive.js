c614026b14d523ddc43727205abbcfde1c07aa97
qiaochao <752716886@qq.com>
2021-11-10 16:42:49 +0800

    recursive

--- ps-1-html2json_recursive.js
+++ ps-1-html2json_recursive.js
@@ -1,2 +1,19 @@
+/*==========================================================
+题目逻辑，将一个html字符串转化为json，
+找到其中的video，给video更改和变化一些属性
+然后再将该json转化为html
+============================================================*/
 let {html2json,json2html} = require("html2json");//html to json
+let html = `
+
+
+
+`
+//将html转化为json
+
+//deepClone，找到video属性更改，生成新的json
+
+//将json再转化为html
+
+
 
