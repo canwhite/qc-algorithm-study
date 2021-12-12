40137e66d5ebf4b4a4d31a654e4db7ff73435d2f
qiaochao <752716886@qq.com>
2021-12-12 16:08:14 +0800

    update

---
+++ 13-1-tree-search-广度优先查询.js
@@ -0,0 +1,68 @@
+/*----------------------------------------
+广度优先搜索
+广度优先搜索会优先从离起点近的节点开始搜索
+
+如果从一棵树的角度来看的话，是先宽再高
+----------------------------------------*/
+
+let breadth = (node)=>{
+    let nodes = [];
+    let stack = [];
+    if(node){
+
+        stack.push(node);
+        //如果stack里边有值
+        while(stack.length){
+
+            //从一行里边一个个拿出来children进行下一行
+            let item = stack.shift();
+            let children = item.children || [];
+            nodes.push(item);
+
+            //然后再横向添加一行
+            for(let i = 0; i<children.length;i++){
+                stack.push(children[i]);
+            }
+        }
+    }
+    return nodes;
+}
+//对obj的处理
+let obj = { 
+    children: [
+        { 
+            index: 0, 
+            children: [
+                { 
+                    index: 1, 
+                    children: [{ 
+                        index: 3 
+                    }] 
+                }
+            ] 
+        }, 
+        { 
+            index: 4 
+        }, 
+        { 
+            index: 5, 
+            children: [{ 
+                index: 7, 
+                children: [{ 
+                    index: 8 
+                }] 
+            }] 
+        }, 
+        { 
+            index: 6 
+        }
+    ] 
+}
+let result =  breadth(obj);
+console.log("==result==",result);
+
+
+/*
+
+
+*/
